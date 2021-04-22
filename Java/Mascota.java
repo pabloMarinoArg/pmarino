0dabb072892a1b02c4a61e166b4eb827a85c9c4e
pabloMarinoArg <31575175+pabloMarinoArg@users.noreply.github.com>
2021-04-21 21:05:13 -0300

    Se agrega carpeta java con clase mascota

---
+++ Java/Mascota.java
@@ -0,0 +1,30 @@
+public class Mascota {
+
+	private String nombre;
+	private Integer edad;
+
+	public Mascota(){
+
+	}
+
+	public String getNombre(){
+		return nombre;
+
+	}
+
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+
+	}
+
+	public Integer getEdad(){
+		retrun edad;
+
+	}
+
+	public void setEdad(Integer nombre){
+		this.edad = edad;
+
+	}
+
+}
